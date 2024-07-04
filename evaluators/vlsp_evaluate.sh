f7f752f829fef95027275e3dd08433bdfcc821e4
Quan Dang <18520339@gm.uit.edu.vn>
2024-07-05 00:19:40 +0700

    Update vlsp_evaluate.sh

--- evaluators/vlsp_evaluate.sh
+++ evaluators/vlsp_evaluate.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_DIR="$SCRIPT_DIR/.."
-
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 declare -A domains=( ["Hotel"]="vlsp2018_hotel" ["Restaurant"]="vlsp2018_restaurant" )
 approaches=("v1" "v2")
 
 for domain in "${!domains[@]}"; do
-    dataset="$PROJECT_DIR/datasets/${domains[$domain]}/3-VLSP2018-SA-${domain}-test.txt"
+    test_dataset=$SCRIPT_DIR/../datasets/${domains[$domain]}/3-VLSP2018-SA-$domain-test.txt
     for approach in "${approaches[@]}"; do
-        prediction="$PROJECT_DIR/experiments/predictions/${domain}-${approach}.txt"
-        output="$SCRIPT_DIR/${domain}-${approach}-eval.txt"
-        java "$SCRIPT_DIR"/SAEvaluate.java "$dataset" "$prediction" > "$output"
+        pred_file=$SCRIPT_DIR/../experiments/predictions/$domain-$approach.txt
+        java $SCRIPT_DIR/SAEvaluate.java $test_dataset $pred_file > $SCRIPT_DIR/$domain-$approach-eval.txt
     done
 done
